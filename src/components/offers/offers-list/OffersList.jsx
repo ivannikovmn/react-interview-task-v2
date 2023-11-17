10e2e58a5505d806bf217161f9f606ccc62f0c64
Mikhail <local_host@mail.ru>
2023-11-17 20:27:36 +0600

    user can add or remove only single instance of the Offer by pressing the corresponding button

--- src/components/offers/offers-list/OffersList.jsx
+++ src/components/offers/offers-list/OffersList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Empty, Row, Spin } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { Offer } from "@spanic/react-interview-components";
@@ -8,14 +8,25 @@ import styled from "styled-components";
 
 export default function OffersList() {
   const dispatch = useDispatch();
-
   const { offers, loading } = useSelector((state) => state.offers);
 
-  const OfferWrapper = (offer) => (
+  useEffect(() => {
+    dispatch(fetchOffers());
+  }, [dispatch]);
+
+  const onChangeQty = (id, selectedQty) => {
+    dispatch(changeSelectedQty({ id, selectedQty }));    
+  };
+  
+  const OfferWrapper = ({ id, title, description, price, selectedQty }) => (
     <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }} lg={{ span: 6 }}>
-      <Offer data={offer} />
+      <Offer
+        data={{ id, title, description, price }}
+        onChangeQty={(qty) => onChangeQty(id, qty)}
+        selectedQty={selectedQty}
+      />
     </Col>
-  );
+  ); 
 
   if (loading) {
     return (
@@ -26,7 +37,11 @@ export default function OffersList() {
   }
 
   return offers.length ? (
-    <Row gutter={[16, 16]}>{/* TODO: display the Offers here */}</Row>
+    <Row gutter={[16, 16]}>
+      {offers.map((offer) => (
+        <OfferWrapper key={offer.id} {...offer} onChangeQty={onChangeQty} />
+      ))}
+    </Row>
   ) : (
     <Empty />
   );
--- src/store/offers-slice.js
+++ src/store/offers-slice.js
@@ -12,12 +12,38 @@ export const offersSlice = createSlice({
   initialState,
   reducers: {
     changeSelectedQty: (state, action) => {
-      const { id, selected } = action.payload;
+      const { id, selectedQty } = action.payload;
+
+      console.log("Action Payload:", action.payload);
+
       // TODO: save chosen Offer selected state to the store
+      const selectedOffer = state.offers.find((offer) => offer.id === id);
+
+      if (selectedOffer) {
+        // Обновление выбранного состояния для выбранного предложения        
+        selectedOffer.selectedQty = isNaN(selectedQty) ? 0 : selectedQty;
+
+        // Расчет общей суммы
+        state.totalPrice = state.offers.reduce(
+          (total, offer) => total + offer.price * (offer.selectedQty || 0),
+          0
+        );        
+      }
     },
   },
   extraReducers: (builder) => {
     // TODO: handle asyncThunk state changes here: display/hide loader, add Offers to the store
+    builder
+      .addCase(fetchOffers.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchOffers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.offers = action.payload;
+      })
+      .addCase(fetchOffers.rejected, (state) => {
+        state.loading = false;
+      });
   },
 });
 
